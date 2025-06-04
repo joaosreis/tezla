97f47f1ce956118c296b9e2dd43c74ca4b85759a
João Santos Reis <joaosreis@outlook.pt>
2025-06-04 10:32:52 +0100

    Enhance flake.nix by adding opam-repository inputs and refining package structure

--- flake.nix
+++ flake.nix
@@ -1,12 +1,18 @@
 {
   inputs = {
     opam-nix.url = "github:tweag/opam-nix";
+    opam-nix.inputs.opam-repository.follows = "opam-repository";
     flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.follows = "opam-nix/nixpkgs";
+    opam-repository.url = "github:ocaml/opam-repository";
+    opam-repository.flake = false;
+
   };
-  outputs = { self, flake-utils, opam-nix, nixpkgs }@inputs:
+  outputs = { self, flake-utils, opam-nix, nixpkgs, opam-repository }@inputs:
     # Don't forget to put the package name instead of `throw':
-    let package = "tezla";
+    let
+      packages = [ "tezla-adt" "tezla-converter" ];
+      package = "tezla-converter";
     in flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
@@ -25,16 +31,18 @@
           ## - or force ocamlfind to be a certain version:
           # ocamlfind = "1.9.2";
         };
+        monorepo =
+          on.buildOpamMonorepo { resolveArgs.with-test = true; } ./. query;
         scope =
           on.buildOpamProject { resolveArgs.with-test = true; } package ./.
           query;
         overlay = final: prev: {
-            # Your overrides go here
+          # Your overrides go here
           ocaml-lsp-server = prev.ocaml-lsp-server.overrideAttrs {
             nativeBuildInputs = prev.ocaml-lsp-server.nativeBuildInputs
               ++ [ prev.cppo ];
           };
-          };
+        };
         scope' = scope.overrideScope overlay;
         # The main package containing the executable
         main = scope'.${package};
@@ -42,7 +50,8 @@
         devPackages = builtins.attrValues
           (pkgs.lib.getAttrs (builtins.attrNames devPackagesQuery) scope');
       in {
-        legacyPackages = scope';
+        # for each package in packages
+        lib.legacyPackages = scope';
 
         packages.default = self.legacyPackages.${system}.${package};
 
@@ -50,6 +59,7 @@
           inputsFrom = [ main ];
           buildInputs = devPackages ++ [
             pkgs.nixd
+            pkgs.nixfmt
             # You can add packages from nixpkgs here
           ];
         };
